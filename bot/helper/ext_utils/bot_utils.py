ae2fa1eef44f248f730213a23aeddb1abaa5d7a1
ViSHWA14510 <154061854+ViSHWA14510@users.noreply.github.com>
2023-12-21 01:31:56 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -141,10 +141,10 @@ def get_progress_bar_string(pct):
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
     cPart = int(p % 8 - 1)
-    p_str = '▰' * cFull
+    p_str = '●' * cFull
     if cPart >= 0:
-        p_str += ['▱', '🦋', '▱', '🦋', '▱', '🦋', '▰'][cPart]
-    p_str += '▱' * (12 - cFull)
+        p_str += ['○', '🦋', '○', '🦋', '○', '🦋', '●'][cPart]
+    p_str += '○' * (12 - cFull)
     return f"∊{p_str}∋"
 
 
